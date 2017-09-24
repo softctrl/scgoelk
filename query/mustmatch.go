c070b2c3d676ade9f6c3011d3eb1241587747f07
Timoshenko <softctrl@users.noreply.github.com>
2017-09-24 18:07:14 -0300

    Add files via upload

---
+++ query/mustmatch.go
@@ -0,0 +1,33 @@
+//
+// Author:
+//  Carlos Timoshenko
+//  carlostimoshenkorodrigueslopes@gmail.com
+//
+//  https://github.com/softctrl
+//
+// This project is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+package query
+
+//import (
+//	"encoding/json"
+//)
+
+////
+//// Based on:
+//// {"match":{"fn":"Anderson edson"}}
+////
+//type match struct {
+//}
+
+//func Match() {
+
+//}
