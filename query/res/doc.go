8e3555234903944d2efe893f68ed005b1db06566
Timoshenko <softctrl@users.noreply.github.com>
2017-09-24 18:11:01 -0300

    Add files via upload

---
+++ query/res/doc.go
@@ -0,0 +1,46 @@
+//
+// Author:
+//  Carlos Timoshenko
+//  carlostimoshenkorodrigueslopes@gmail.com
+//
+//  https://github.com/softctrl
+//
+// This project is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+//
+// Based on the elasticsearch result
+// {
+// 	"st": 0,
+// 	"body": {
+// 		"took": 24,
+// 		"timed_out": false,
+// 		"_shards": {
+// 			"total": 5,
+// 			"successful": 5,
+// 			"failed": 0
+// 		},
+// 		"hits": {
+// 			"total": 0,
+// 			"max_score": null,
+// 			"hits": [
+// 				{
+//					"_index": "redis",
+//					"_type": "pessoa",
+//					"_id": "-2009290007",
+//					"_score": 13.137769,
+//					"_source": { ... }
+//				}
+// 			]
+// 		}
+// 	}
+// }
+//
+package res
